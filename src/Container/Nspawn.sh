985df894064c04b59c52b024d6946195aa4fdce2
chongwish <chongwish@t440s.local>
2023-03-01 19:00:52 +0800

    feature: casually mount/umount/cd

--- src/Container/Nspawn.sh
+++ src/Container/Nspawn.sh
@@ -29,3 +29,28 @@ is_mounted() {
 exec_command() {
     machinectl shell -q $BASHERT_CONTAINER_NSPAWN_NAME /bin/sh -c "$@"
 }
+
+find_mount_disk() {
+    has_name
+
+    var disk_path="`File.locate_disk_path "$1"`"
+
+    local disk_name="${disk_path%%:*}"
+    local relation_path="${disk_path#*:}"
+
+    local mount_name
+    mount_name="`exec_command "findmnt -ln -a -o TARGET,SOURCE | sed -rn 's|(.*)[[:space:]]+($disk_name)\[(/@rootfs)?$relation_path\]|\1|p'"`"
+    mount_name="$(echo "$mount_name" | sed `printf 's/\s*\r$//g'`)"
+
+    ret $mount_name
+}
+
+mount_disk() {
+    :
+}
+
+umount_disk() {
+    has_name
+
+    exec_command "umount '$1' && rmdir '$1'"
+}
