7af619d4c812758780da99213d27722b51e5e9fe
chongwish <chongwish@gmail.com>
2024-08-06 18:18:52 +0800

    support wayland in nspawn container

--- src/Container/Nspawn/Desktop.sh
+++ src/Container/Nspawn/Desktop.sh
@@ -2,55 +2,73 @@
 BASHERT_CONTAINER_NSPAWN_DESKTOP_UID="`id -u`"
 
 run() {
+    local desktop_env=""
+    local display_env=${DISPLAY:-":0.0"}
+    local xdg_user_dir_path="/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
+
+    var container_name="`Container.Nspawn.get_name`"
+
+    Container.Nspawn.exec_command "[ -e $xdg_user_dir_path ] || systemctl start user@$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
+
+    # ENV XDG
+    desktop_env="$desktop_env XDG_RUNTIME_DIR=$xdg_user_dir_path"
+
+    # ENV PULSE
+    desktop_env="$desktop_env PULSE_SERVER=$xdg_user_dir_path/pulse-native"
+    
+    # ENV DBUS
+    desktop_env="$desktop_env DBUS_SESSION_BUS_ADDRESS=unix:path=$xdg_user_dir_path/bus"
+
+    # ENV WAYLAND
+    desktop_env="$desktop_env WAYLAND_DISPLAY=$WAYLAND_DISPLAY"
+
+    # ENV DISPLAY
+    desktop_env="$desktop_env DISPLAY=${display_env}"
+
+    create_socket_wayland
+    create_socket_pulse
+    create_socket_dbus
+
     # Don't use $XAUTHORITY here
     if [ -z "`xhost | grep localuser:$(whoami)`" ]; then
         xhost +local:
     fi
 
-    create_socket_pulse
-    create_socket_dbus
-
-    local desktop_env
-    var container_name="`Container.Nspawn.get_name`"
-
-    Container.Nspawn.exec_command "[ -e /run/user/BASHERT_CONTAINER_NSPAWN_DESKTOP_UID ] || systemctl start user@$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
-
-    # ENV DISPLAY
-    local display_env=${DISPLAY:-":0.0"}
-    desktop_env="$desktop_env DISPLAY=${display_env}"
-
-    # ENV PULSE
-    desktop_env="$desktop_env PULSE_SERVER=\$HOME/.pulse-native"
-    
-    # ENV DBUS
-    desktop_env="$desktop_env DBUS_SESSION_BUS_ADDRESS=unix:path=\$HOME/.bus"
-
-    # ENV XDG
-    desktop_env="$desktop_env XDG_RUNTIME_DIR=/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
-
     machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc "$desktop_env $@"
+}
+
+create_socket_wayland() {
+    local container_user_home
+    local socket_path
+    local xdg_user_dir_path="/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
+    #container_user_home="`machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc 'echo -n "$HOME"'`"
+    socket_path="$xdg_user_dir_path/$WAYLAND_DISPLAY"
+    if ! `Container.Nspawn.is_mounted "$socket_path"`; then
+        var container_name="`Container.Nspawn.get_name`"
+        machinectl bind --mkdir "$container_name" "$socket_path" "$socket_path"
+    fi
 }
 
 create_socket_dbus() {
     local container_user_home
     local socket_path
-    container_user_home="`machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc 'echo -n "$HOME"'`"
-    socket_path="$container_user_home/.bus"
-
+    local xdg_user_dir_path="/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
+    #container_user_home="`machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc 'echo -n "$HOME"'`"
+    socket_path="$xdg_user_dir_path/bus"
     if ! `Container.Nspawn.is_mounted "$socket_path"`; then
         var container_name="`Container.Nspawn.get_name`"
-        machinectl bind --mkdir "$container_name" "/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID/bus" "$socket_path"
+        machinectl bind --mkdir "$container_name" "$socket_path" "$socket_path"
     fi
 }
 
 create_socket_pulse() {
     local container_user_home
     local socket_path
-    container_user_home="`machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc 'echo -n "$HOME"'`"
-    socket_path="$container_user_home/.pulse-native"
-
+    local xdg_user_dir_path="/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID"
+    #container_user_home="`machinectl shell -q --uid="$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID" "$container_name" /bin/sh -lc 'echo -n "$HOME"'`"
+    socket_path="$xdg_user_dir_path/pulse-native"
     if ! `Container.Nspawn.is_mounted "$socket_path"`; then
         var container_name="`Container.Nspawn.get_name`"
-        machinectl bind --mkdir "$container_name" "/run/user/$BASHERT_CONTAINER_NSPAWN_DESKTOP_UID/pulse/native" "$socket_path"
+        machinectl bind --mkdir "$container_name" "$xdg_user_dir_path/pulse/native" "$socket_path"
     fi
 }
